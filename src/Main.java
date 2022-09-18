08771bde43ee97347aa60e5685984e4ce990fbed
MRALADAG <mstf_aladag@yahoo.com>
2022-09-18 17:09:15 +0300

    program have been applied which find vowel and consonant using JAVA codes

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,8 @@ public class Main {
 
     public static String chackesliSessizHarf(char letter) {
         return switch (letter) {
-            case 'a', 'ı', 'o', 'u' -> "\nKalın sesli harf\n";
-            case 'e', 'i', 'ö', 'ü' -> "\nİnce sesli harf\n";
+            case 'a', 'ı', 'o', 'u', 'A', 'I', 'O', 'U' -> "\nKalın sesli harf\n";
+            case 'e', 'i', 'ö', 'ü', 'E', 'İ', 'Ö', 'Ü' -> "\nİnce sesli harf\n";
             default -> "\nGeçersiz harf veya karakter girişi yaptınız!\n ";
         };
     }
